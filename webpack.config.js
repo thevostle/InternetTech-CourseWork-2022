7407ed1d4f2ef01381871d304837e916969cd7cc
Eltsov Dmitriy <technic.dima@yandex.ru>
2022-04-11 20:51:07 +0300

    перенастроил сборщик

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     mode: 'development',
     entry: {
-        main: "./static/js/SomePage.js",
+        main: "./static/js/MainPage.js",
     },
     module: {
         rules: [
