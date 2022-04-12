f530502859928e419f1dacbe7f69e789886d08a5
thevostle <technic.dima@gmail.com>
2022-04-13 00:15:56 +0300

    создал компонент формы авторизации и карточку кино

--- static/js/MainPage.js
+++ static/js/MainPage.js
@@ -1,5 +1,6 @@
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import FilmCard from "./components/cards/FilmCard";
 import AuthComponent from "./components/AuthComponent";
 
 import React from "react";
